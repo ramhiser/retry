c9b802c843482e953105e0f9520152acf12d4b01
John Ramey <johnramey@gmail.com>
2016-03-29 21:25:50 -0500

    Stubbed retry_f. A few docs too.

--- R/retry.r
+++ R/retry.r
@@ -1,7 +1,20 @@
-retry <- function(expr, config) {
-  NULL
+#' Retry a function
+#'
+#' The function \code{f} is given retry behavior based on the specification in
+#' the \code{config} configuration list.
+#'
+#' The default behavior is to retry the function \code{f} forever without
+#' waiting.
+#'
+#' @export
+#' @param f function to add retry behavior
+#' @param config named list to specify retry behavior
+#' @return a function with retry behavior added to the function \code{f}
+retry_f <- function(f, config) {
+  f()
 }
 
-retry_f <- function(f, config) {
+#' Retry expression
+retry <- function(expr, config) {
   NULL
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # retry
 R Package to Retry Any Expression or Function
+
+## Config Options
+
+* Number of Retries
+* Max Runtime of Retries
+* Time between Retries
