ae59b2246f86213673fe28ea782b7ed9750065ec
John Ramey <johnramey@gmail.com>
2016-03-31 21:48:39 -0500

    Stubbed unit tests.

---
+++ tests/testthat/test-retry.r
@@ -0,0 +1,13 @@
+context("Retry")
+
+test_that("Stop conditions work", {
+  expect_true(FALSE)
+})
+
+test_that("Wait conditions work", {
+  expect_true(FALSE)
+})
+
+test_that("Runtime conditions work", {
+  expect_true(FALSE)
+})
